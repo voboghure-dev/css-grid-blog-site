01810c78e498b5ee00ab033c1e228454cf095a6f
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-06-09 12:46:54 +0600

    Footer css done
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const mobileMenu = document.querySelector('.mobile-menu');
-mobileMenu.addEventListener('click', ()=>{
+mobileMenu.addEventListener('click', () => {
   document.querySelector('.menu').classList.toggle('show');
+});
+
+const scrollBtn = document.querySelector('.scroll-top');
+window.addEventListener('scroll', function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollBtn.style.display = 'block';
+  } else {
+    scrollBtn.style.display = 'none';
+  }
+});
+
+scrollBtn.addEventListener('click', () => {
+  document.documentElement.scrollTop = 0;
 });
